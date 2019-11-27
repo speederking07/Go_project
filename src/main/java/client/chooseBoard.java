a65f003d0def1b75aac1dc35dd57df0f117ee130
Szakul1 <56487624+Szakul1@users.noreply.github.com>
2019-11-28 00:31:01 +0100

    added opponent choice

--- src/main/java/client/chooseBoard.java
+++ src/main/java/client/chooseBoard.java
@@ -3,36 +3,69 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class chooseBoard extends JFrame implements ActionListener
+public class chooseBoard extends JFrame implements ActionListener
 {
     JLabel chooseBoardJLabel;
-    JButton JButton9x9;
-    JButton JButton13x13;
-    JButton JButton19x19;
+    JLabel choosePlayerJLabel;
+    JLabel statusJLabel;
+    JLabel statusInfoJLabel;
+    JLabel displayHelpJLabel;
+    JToggleButton JToggleButton9x9;
+    JToggleButton JToggleButton13x13;
+    JToggleButton JToggleButton19x19;
+    JToggleButton botJToggleButton;
+    JToggleButton playerJToggleButton;
+    JButton playJButton;
+    boolean sizeSelected=false;
+    boolean playerSelected=false;
+    int boardSize;
     public chooseBoard()
     {
         chooseBoardJLabel = new JLabel("Wybierz rozmiar",JLabel.CENTER);
+        choosePlayerJLabel = new JLabel("Wybierz przeciwnika",JLabel.CENTER);
+        statusJLabel = new JLabel("Status",JLabel.CENTER);
+        statusInfoJLabel = new JLabel("",JLabel.CENTER);
+        displayHelpJLabel=new JLabel();
 
-        JButton9x9=new JButton("9x9");
-        JButton13x13=new JButton("13x13");
-        JButton19x19=new JButton("19x19");
+        JToggleButton9x9=new JToggleButton("9x9");
+        JToggleButton13x13=new JToggleButton("13x13");
+        JToggleButton19x19=new JToggleButton("19x19");
+        botJToggleButton=new JToggleButton("Bot");
+        playerJToggleButton=new JToggleButton("Gracz");
+        playJButton=new JButton("Graj");
 
         chooseBoardJLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        choosePlayerJLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        statusJLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        statusInfoJLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
 
-        JButton9x9.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
-        JButton13x13.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
-        JButton19x19.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        JToggleButton9x9.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        JToggleButton13x13.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        JToggleButton19x19.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 30));
+        botJToggleButton.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
+        playerJToggleButton.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
+        playJButton.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
 
-        JButton9x9.addActionListener(this);
-        JButton13x13.addActionListener(this);
-        JButton19x19.addActionListener(this);
+        JToggleButton9x9.addActionListener(this);
+        JToggleButton13x13.addActionListener(this);
+        JToggleButton19x19.addActionListener(this);
+        botJToggleButton.addActionListener(this);
+        playerJToggleButton.addActionListener(this);
+        playJButton.addActionListener(this);
 
         add(chooseBoardJLabel);
-        add(JButton9x9);
-        add(JButton13x13);
-        add(JButton19x19);
+        add(choosePlayerJLabel);
+        add(statusJLabel);
+        add(JToggleButton9x9);
+        add(botJToggleButton);
+        add(statusInfoJLabel);
+        add(JToggleButton13x13);
+        add(playerJToggleButton);
+        add(displayHelpJLabel);
+        add(JToggleButton19x19);
+        add(playJButton);
 
-        setLayout(new GridLayout(4,1));
+        setLayout(new GridLayout(4,3));
         pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,19 +76,61 @@ class chooseBoard extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         if(e.getActionCommand().equals("9x9"))
-            searchPlayers(9);
+        {
+            JToggleButton13x13.setSelected(false);
+            JToggleButton19x19.setSelected(false);
+            if(JToggleButton9x9.isSelected())
+                sizeSelected=true;
+            else 
+                sizeSelected=false;
+            boardSize=9;
+        }
         if(e.getActionCommand().equals("13x13"))
-            searchPlayers(13);
+        {
+            JToggleButton9x9.setSelected(false);
+            JToggleButton19x19.setSelected(false);
+            if(JToggleButton13x13.isSelected())
+                sizeSelected=true;
+            else 
+                sizeSelected=false;
+            boardSize=13;
+        }
         if(e.getActionCommand().equals("19x19"))
-            searchPlayers(19);
-
+        {
+            JToggleButton9x9.setSelected(false);
+            JToggleButton13x13.setSelected(false);
+            if(JToggleButton19x19.isSelected())
+                sizeSelected=true;
+            else 
+                sizeSelected=false;
+            boardSize=19;
+        }
+        if(e.getActionCommand().equals("Bot"))
+        {
+            playerJToggleButton.setSelected(false);
+            if(botJToggleButton.isSelected())
+                playerSelected=true;
+            else
+                playerSelected=false;
+        }
+        if(e.getActionCommand().equals("Gracz"))
+        {
+            botJToggleButton.setSelected(false);
+            if(playerJToggleButton.isSelected())
+                playerSelected=true;
+            else
+                playerSelected=false;
+        }
+        if(e.getActionCommand().equals("Graj"))
+        {
+            if(sizeSelected && playerSelected)
+            {
+                setVisible(false);
+                new playGo(boardSize);
+            }
+        }
     }
 
-    private void searchPlayers(int boardSize)
-    {
-        setVisible(false);
-        new playGo(boardSize);
-    }
 
     public static void main(String args[])
     {
